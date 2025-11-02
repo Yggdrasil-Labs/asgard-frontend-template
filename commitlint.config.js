1a522d92e3e3da54c49d661d462033120463b786
YoungerYang <1059376437@qq.com>
2025-11-02 21:33:56 +0800

    chore: 提交时不强制要求绑定issue

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,6 @@ export default {
   rules: {
     'body-max-line-length': [1, 'always', 500], // 改为警告级别 + 120字符
     // 要求提交信息必须包含至少一个 issue 引用（如 #123、Closes #123、Fixes #123 等）
-    'references-empty': [2, 'never'],
+    // 'references-empty': [2, 'never'],
   },
 }
